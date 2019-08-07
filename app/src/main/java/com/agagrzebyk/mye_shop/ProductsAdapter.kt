d658e264bc1ac0d6a013c70823f1ab7ebc6b8e47
aga.grzebyk <aga.grzebyk@gmail.com>
2019-08-07 23:08:35 +0200

    Initial commit

---
+++ app/src/main/java/com/agagrzebyk/mye_shop/ProductsAdapter.kt
@@ -0,0 +1,32 @@
+package com.agagrzebyk.mye_shop
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.agagrzebyk.mye_shop.ProductsAdapter.ViewHolder
+import com.agagrzebyk.mye_shop.model.Product
+
+class ProductsAdapter(private val products: ArrayList<Product>): RecyclerView.Adapter<ProductsAdapter.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.title.text = products[position].title
+        TODO("not implemented")
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val View = LayoutInflater.from(parent.context).inflate(R.layout.product_row, parent, false)
+        return ViewHolder(View)
+    }
+
+    override fun getItemCount() = products.size
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val image: ImageView = itemView.findViewById(R.id.photo)
+        val title: TextView = itemView.findViewById(R.id.title)
+
+    }
+
+}
---
+++ app/src/main/java/com/agagrzebyk/mye_shop/model/Product.kt
@@ -0,0 +1,7 @@
+package com.agagrzebyk.mye_shop.model
+
+data class Product(
+    val title: String,
+    val photoUrl: String,
+    val price: Double
+)
