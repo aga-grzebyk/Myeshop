4735a7401e953b750486cd92fbdcde29281a949b
aga.grzebyk <aga.grzebyk@gmail.com>
2019-08-12 23:13:49 +0200

    Nawigation drawer created

--- app/src/main/java/com/agagrzebyk/mye_shop/MainActivity.kt
+++ app/src/main/java/com/agagrzebyk/mye_shop/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.agagrzebyk.mye_shop
 
 import android.os.Bundle
+import android.util.Log.d
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.view.GravityCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.agagrzebyk.mye_shop.model.Product
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +21,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        navigationView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.actionHome -> d("daniel", "Going home")
+                R.id.actionJewelry -> d("daniel", "fancy earrings with dimonds")
+                R.id.actionEngagement -> d("daniel", "Will You merry me")
+                R.id.actionWatches -> d("daniel", "What time is it?")
+                R.id.actionAccessories -> d("daniel", "Happy Birthday!")
+            }
+            it.isChecked = true
+            drawerLayout.closeDrawers()
+            true
+        }
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
+        }
 
         val products = arrayListOf<Product>()
 
@@ -31,4 +52,12 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        drawerLayout.openDrawer(GravityCompat.START)
+     //   return true
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
