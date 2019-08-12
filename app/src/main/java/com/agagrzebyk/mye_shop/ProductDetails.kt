3a6e37fcaf14ad4f9f88d027533ad90c0383ecd4
aga.grzebyk <aga.grzebyk@gmail.com>
2019-08-12 16:30:06 +0200

    Product details with photo and button

--- app/src/main/java/com/agagrzebyk/mye_shop/ProductDetails.kt
+++ app/src/main/java/com/agagrzebyk/mye_shop/ProductDetails.kt
@@ -1,8 +1,10 @@
 package com.agagrzebyk.mye_shop
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.product_details.*
 
@@ -14,5 +16,14 @@ class ProductDetails : AppCompatActivity(){
 
         val title = intent.getStringExtra("title")
         product_name.text = title
+
+        availability.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setMessage("Hey, $title is in stock")
+                .setPositiveButton("OK") { p0, p1 -> }
+                .create()
+                .show()
+        }
+
     }
 }
