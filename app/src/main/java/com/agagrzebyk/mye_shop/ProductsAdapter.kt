27f2c6c58d3dfb48902947856679c04db33178f9
aga.grzebyk <aga.grzebyk@gmail.com>
2019-08-15 23:56:41 +0200

    SALE sign added

--- app/src/main/java/com/agagrzebyk/mye_shop/ProductsAdapter.kt
+++ app/src/main/java/com/agagrzebyk/mye_shop/ProductsAdapter.kt
@@ -10,16 +10,22 @@ import androidx.recyclerview.widget.RecyclerView
 import com.agagrzebyk.mye_shop.ProductsAdapter.ViewHolder
 import com.agagrzebyk.mye_shop.model.Product
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.product_row.view.*
 
 class ProductsAdapter(private val products: List<Product>):
     RecyclerView.Adapter<ProductsAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ProductsAdapter.ViewHolder, position: Int) {
-
         val product = products[position]
         Picasso.get().load(product.photoUrl).into(holder.image)
         holder.title.text = product.title
         holder.price.text = product.price.toString()
+
+        if(product.isOnSale){
+            holder.saleImageView.visibility = View.VISIBLE
+        } else {
+            holder.saleImageView.visibility = View.GONE
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -41,5 +47,6 @@ class ProductsAdapter(private val products: List<Product>):
         val image: ImageView = itemView.findViewById(R.id.photo)
         val title: TextView = itemView.findViewById(R.id.title)
         val price: TextView = itemView.findViewById(R.id.price)
+        val saleImageView : ImageView = itemView.saleImageView
     }
 }
