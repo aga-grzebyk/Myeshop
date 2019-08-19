43f9fe84d819f3584cc35fab56e9df7a0ba3892f
aga.grzebyk <aga.grzebyk@gmail.com>
2019-08-19 23:01:33 +0200

    working on admin fragment

--- app/src/main/java/com/agagrzebyk/mye_shop/AdminFragment.kt
+++ app/src/main/java/com/agagrzebyk/mye_shop/AdminFragment.kt
@@ -1,13 +1,44 @@
 package com.agagrzebyk.mye_shop
 
 import android.os.Bundle
+import android.util.Log.d
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.room.Room
+import com.agagrzebyk.mye_shop.Database.AppDatabase
+import com.agagrzebyk.mye_shop.Database.ProductFromDatabase
+import kotlinx.android.synthetic.main.fragment_admin.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 class AdminFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_admin, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        submitButton.setOnClickListener {
+            val title = productTitle.text
+            d("aga", "Button pressed with text of $title")
+
+            doAsync {
+
+                val db = Room.databaseBuilder(
+                    activity!!.applicationContext,
+                    AppDatabase::class.java, "database-name"
+                ).build()
+
+                db.productDao().insertAll(
+                    ProductFromDatabase(null, title.toString() , 75.99))
+
+                uiThread {
+d("aga", "Product added to database")
+                }
+            }
+        }
     }
 }
